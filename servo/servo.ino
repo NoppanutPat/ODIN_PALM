881c3a2f543d2da9f2e73f75b99b9db605b9d9b5
ThongtonN <noppanut_pkw@live.com>
2018-07-19 10:38:12 +0700

    .\servo.ino

---
+++ servo/servo.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+#define servo 9
+Servo myservo;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(servo);
+  myservo.write(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myservo.write(180);
+  delay(1000);
+  myservo.write(90);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+}
